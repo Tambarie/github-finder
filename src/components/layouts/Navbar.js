20f6d40b5fcbbdde640e7ec1bfb95953357af808
tambarie <gt.gbaragbo@gmail.com>
2021-11-10 01:35:25 +0100

    add Search component

---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { PropTypes } from 'prop-types'
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+    </nav>
+  )
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+}
+Navbar.defaultProps = {
+  title: 'Github-Finder',
+  icon: 'fab fa-github',
+}
+export default Navbar
